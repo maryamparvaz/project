b708ed3c42c397cb11dee81820410477f3890827
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-16 22:39:15 +0330

    Add files via upload

---
+++ avrage.js
@@ -0,0 +1,6 @@
+let x =Number(prompt('add aval ra varedd konid'));
+let y=Number(prompt('add dovom ra varedd konid'));
+let z =Number(prompt('add sevom ra varedd konid'));
+let avrage = (x + y + z) / 3;
+alert(avrage);
+
