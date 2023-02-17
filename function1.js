a9c93786501591719d2beb1e7fa3f52495d7c3bd
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-17 12:59:09 +0330

    Add files via upload

---
+++ function1.js
@@ -0,0 +1,12 @@
+// تابعی بنویسید که عددی را دریافت کرده و زوج یا فرد بودن آن را نمایش دهد
+
+const x = function () {
+    vorodi = Number(prompt('yek add vared konid'))
+    if (vorodi % 2 == 0) {
+        alert ('add zog ast')
+    } else {
+        alert('add fard ast')
+     }
+}
+
+x();
