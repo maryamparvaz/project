193d6f23a36811a329712a7dfe3ec59d809a5096
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-17 18:43:06 +0330

    Add files via upload

---
+++ tolowercase.js
@@ -0,0 +1,6 @@
+let user = prompt('username khod ra vared konid');
+if (user.toLowerCase() == 'ali') {
+    alert('shoma mitavanid vared shavid')
+} else {
+    alert('shoma mogaz be vorod nistid')
+}
