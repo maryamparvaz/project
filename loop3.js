b02a326fa3136cc172aaddf9fb60f569bb298824
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-17 22:20:39 +0330

    Add files via upload

---
+++ loop3.js
@@ -0,0 +1,11 @@
+// عدد را از کاربر گرفته و میانگین انهارا نشان دهید5
+
+avvrage = 0;
+    
+
+for (let i = 0; i < 5; i++){
+    avvrage = avvrage + Number(prompt('add '+ (i+1)    + 'vared konid'))
+   
+    
+}
+alert(avvrage/5)
