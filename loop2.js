7d0384a9934f3e8405d864beff959b2e9d344acd
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-17 21:54:59 +0330

    Add files via upload

---
+++ loop2.js
@@ -0,0 +1,9 @@
+// قیمت پنج محصول را از کاربر گرفته و قیمت کل سبد خرید را محاسبه کنید
+
+
+let price = 0;
+
+for (let i = 0; i < 5; i++){
+    price =price+ Number(prompt('gheimat  ra vared konid'))
+
+}alert(price)
