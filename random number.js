591e801bcc19d413fbdc9eee4ff280d890b83092
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-17 19:00:51 +0330

    Add files via upload

---
+++ random number.js
@@ -0,0 +1,8 @@
+// برنامه ای بنویسید که کد کپچای 5 رقمی تولید کند
+
+let randomm = Math.random() * 100000
+// هرتعداد رقمی ک میخوایم باشه ب همون اندازخ جلوش 0 میذاریم
+
+let code = Math.floor(randomm);
+
+console.log(code)
