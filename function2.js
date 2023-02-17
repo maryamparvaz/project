e8418ec182ebe9b92fd46ac3ef08ad9bce2512ac
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-17 13:12:27 +0330

    Add files via upload

---
+++ function2.js
@@ -0,0 +1,10 @@
+// تابعی بنویسید که سه عدد رو بگیره و میانگین اونا رو محاسبه کرده و نشون بده
+
+function avrage() {
+    x = Number(prompt('add aval ra vared konid'));
+    y = Number(prompt('add dovom ra vared konid'));
+    z = Number(prompt('add sevom ra vared konid'));
+    let avvrage = (x + y + x) / 3;
+    alert(avvrage);
+}
+avrage()
