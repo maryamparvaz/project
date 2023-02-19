90c4f03c6aaed26b6fd9c9d10bc5c78409f1d153
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-19 09:10:31 +0330

    Add files via upload

---
+++ while2.js
@@ -0,0 +1,9 @@
+// با استفاده از  while دو عدد را از کاربر بگیرید و اعداد زوج میان ان دو را نشان دهید
+
+let number1 = Number(prompt('Enter your first number'))
+let number2 = Number(prompt('Enter your second number'))
+while (number1<number2) {
+    console.log(number1)
+    number1+=2
+}
+
