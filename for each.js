ccba86c36334b7590e9f324548beb21b2b7aea14
maryamparvaz <124708513+maryamparvaz@users.noreply.github.com>
2023-02-23 22:32:37 +0330

    Add files via upload

---
+++ for each.js
@@ -0,0 +1,13 @@
+// دیتابیسی برای ذخبره اطلاعات کاربران داشته باشید(ارایه ای از ابجکت ها)
+// و سپس ب کمک متد فور ایچ هر یوزر را به صورت جداگانه در کنسول نمایش دهید
+let users = [
+    { id: 1, name: 'ali', age: 23 },
+    { id: 2, name: 'mary', age: 21 },
+    { id: 3, name: 'amir', age: 25 },
+    { id: 4, name: 'mahdi', age: 29 }
+]
+
+users.forEach(function (user) {
+    
+    console.log(user)
+})
